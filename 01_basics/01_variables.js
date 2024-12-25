e36b1e471f73113c84f579f8ae45e8d47f4cbff9
Prince <135591722+Princekushwaha001@users.noreply.github.com>
2024-12-25 10:00:46 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+// const accountId = 144553
+// let accounntEmail = "prince@google.com"
+// var accountPassward = "12345"
+// accountCity = "Jaipur"
+
+// //accountId = 2
+
+// console.log(accountId);
+
+// console.table([accountId,accounntEmail,accountPassward,accountCity])
+
+"use strict"; // treat all js code as newer version
+
+console.log(typeof "Prince");
+console.log(typeof null);
+console.log(typeof undefined);
